70d54935e14af20a00a4dbdfa5058f4c95d8edcc
mohamed zakaria <mohamed_zakaria@gmail.com>
2026-01-25 19:02:34 +0200

    Add navbar to index and fix bug in form error href to index.html

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-console.log("hello");
+const userId = localStorage.getItem("userId");
+
+if (userId !== null) {
+  const tagLogout = document.getElementById("tagLogout");
+  tagLogout.classList.remove("d-none");
+
+  const apiUrl = `https://homunityapiv1.runasp.net/api/Users/Get Profile By ID?id=${userId}`;
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) throw new Error("حدث خطأ أثناء جلب البيانات");
+      return response.json();
+    })
+    .then((user) => {
+      // بناء بطاقة المستخدم
+      const container = document.getElementById("iconeText");
+      container.innerHTML = `
+        <div class="user-card">
+          <h4 class="text-warning"><span class="text-black ">hello</span> ${user.firstName}</h4>
+        </div>
+      `;
+    });
+} else {
+}
+
+document.getElementById("buttonLogout").addEventListener("click", function () {
+  localStorage.removeItem("userId");
+  window.location.href = "index.html";
+});
